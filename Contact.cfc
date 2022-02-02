7889365d3ddd5d7b34b9be225ad8aa326e9acfea
vajraJvalita <aafehr@gmail.com>
2022-02-02 14:30:13 -0600

    Apply contact form to site

--- Contact.cfc
+++ Contact.cfc
@@ -4,9 +4,12 @@ component extends="Master" {
 	**/
 	remote any function sendInfo() {
 		var data = getJson();
+		if(!data.name.len()){
+			return "invalid-name";
+		}		
 		if(!isValid("email", data.email)){
-			cfheader(statusCode=400, statusText="Invalid Email...");
-			return false;
+			/* cfheader(statusCode=400, statusText="Invalid Email..."); */
+			return "invalid-email";
 		}
 		var message = "The customer submitted the following info:<br><br>Name: #data.name#<br>Email: #data.email# <br> Message: #data.message#";
 		sendEmail(to = "aafehr@gmail.com", subject = "Someone filled out the contact request form", body = message);
--- scripts.js
+++ scripts.js
@@ -93,7 +93,6 @@ animationObserver('.card2', 'card2-ani');
 animationObserver('.card3', 'card3-ani');
 
 
-
 /***********************************************/
 /***************  Scroll Reload  ***************/
 /***********************************************/
@@ -105,4 +104,70 @@ if (history.scrollRestoration) {
         window.scrollTo(0, 0);
     }
 }
+
+
+
+/***********************************************/
+/****************  Contact Form  ***************/
+/***********************************************/
+function handleResponse(reply) {
+	var popForm = document.getElementById("pop-form");
+	var popSuccess = document.getElementById("pop-success");
+	var popTitle = document.getElementById("pop-title");
+	
+	if(reply == "success") {
+		popForm.classList.add("displayNone");
+		popSuccess.classList.remove("displayNone");
+		popSuccess.classList.add("fadeIn");
+		popTitle.innerHTML = "Message Sent Successfully!";
+	}
+	
+	if(reply == "invalid-name") {
+		document.getElementById("name-label").innerHTML = "Name: <span class='error-text'>Required</span>";
+		document.getElementById("name").classList.add("red-border");
+	} else {
+		document.getElementById("name-label").innerHTML = "Name:";
+		document.getElementById("name").classList.remove("red-border");
+	}
+	
+	if(reply == "invalid-email") {
+		document.getElementById("email-label").innerHTML = "Email: <span class='error-text'>Invalid Email Address</span>";
+		document.getElementById("email").classList.add("red-border");
+	} else {
+		document.getElementById("email-label").innerHTML = "Email:";
+		document.getElementById("email").classList.remove("red-border");
+	}
+}
+function handleSubmit(event) {
+	event.preventDefault();
+
+	const data = new FormData(event.target);
+
+	const value = Object.fromEntries(data.entries());
+	let url =
+	  'http://dev.easy-apps.ca:3000/contact.cfc?method=sendInfo&returnFormat=JSON';
+	postData(url, value).then((response) => {
+	  //This runs afer the email has been sent. So you  can do whatever is needed here.
+	  handleResponse(response);
+	});
+}
+async function postData(url = '', data = {}) {
+	// Default options are marked with *
+	const response = await fetch(url, {
+	  method: 'POST', // *GET, POST, PUT, DELETE, etc.
+	  mode: 'cors', // no-cors, *cors, same-origin
+	  cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+	  credentials: 'same-origin', // include, *same-origin, omit
+	  headers: {
+		'Content-Type': 'application/json',
+		// 'Content-Type': 'application/x-www-form-urlencoded',
+	  },
+	  redirect: 'follow', // manual, *follow, error
+	  referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+	  body: JSON.stringify(data), // body data type must match "Content-Type" header
+	});
+	return response.json(); // parses JSON response into native JavaScript objects
+}
+const form = document.querySelector('form');
+form.addEventListener('submit', handleSubmit);
 
