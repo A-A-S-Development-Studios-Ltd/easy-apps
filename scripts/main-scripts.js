c87cda0703f5945efa60fae0c9ea7123d969234b
vajraJvalita <aafehr@gmail.com>
2022-02-02 23:09:20 -0600

    About page

--- scripts/main-scripts.js
+++ scripts/main-scripts.js
@@ -59,41 +59,6 @@ function closeOverlay() {
 
 
 /***********************************************/
-/************  Animation Observers  ************/
-/***********************************************/
-
-function animationObserver (observedObject, animationClass) {
-	const obs = new IntersectionObserver(entries => {
-		entries.forEach(entry => {
-			if (entry.isIntersecting) {
-			  entry.target.classList.add(animationClass);
-			}
-		});
-	});
-
-	obs.observe(document.querySelector(observedObject));
-}
-
-//Hero Banners
-animationObserver('.videoBanner', 'video-banner-ani');
-animationObserver('.bg-dots-blue-video', 'bg-dots-blue-ani');
-
-//Feature Box Observers
-animationObserver('.box1', 'box1-ani');
-animationObserver('.box2', 'box2-ani');
-animationObserver('.box3', 'box3-ani');
-animationObserver('.box4', 'box4-ani');
-
-//Bg Triangles
-animationObserver('.bg-triangles', 'bg-tri-ani');
-
-//Feature Card Observers
-animationObserver('.card1', 'card1-ani');
-animationObserver('.card2', 'card2-ani');
-animationObserver('.card3', 'card3-ani');
-
-
-/***********************************************/
 /***************  Scroll Reload  ***************/
 /***********************************************/
 
