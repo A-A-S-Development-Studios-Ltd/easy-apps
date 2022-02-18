4b896480fab2d3bd258998821d668c1fe9fb4c22
vajraJvalita <aafehr@gmail.com>
2022-02-17 23:32:44 -0600

    Added data layer push events

--- scripts/main-scripts.js
+++ scripts/main-scripts.js
@@ -80,6 +80,58 @@ function scrollPage(target) {
 }
 
 
+/***********************************************/
+/**************  Data layer Click  *************/
+/***********************************************/
+
+window.dataLayer = window.dataLayer || [];
+
+function eventClick(clickedObject, redirectTarget) {
+	
+	/* Analtyics Info */
+	var pageName = window.location.pathname;		
+	if(pageName == '/'){ pageName = "Home";	}
+	var userBrowser = navigator.appVersion;
+	console.log(userBrowser);
+	
+	dataLayer.push({
+		'event': 'user-action',
+		'eventLocation': pageName,
+		'eventName': clickedObject,
+		'userBrowser': userBrowser,
+	});
+	
+	/*Redirects*/
+	if (redirectTarget == "showOverlay") {
+		showOverlay();	}
+	
+	if (redirectTarget == "closeOverlay") {
+		closeOverlay();	}
+	
+	if (redirectTarget == "homeScroll-1") {
+		scrollPage('home-section1');	}
+	
+	if (redirectTarget == "services") {
+		window.location.href = "/services.html";	}
+	
+	if (redirectTarget == "easyPDF") {
+		window.open('https://easypdfsigner.ca/', '_blank').focus();	}
+	
+	if (redirectTarget == "easyTodo") {
+		window.open('https://qa.easy-apps.ca/easytodo/app/login', '_blank').focus();	}
+	
+	if (redirectTarget == "buginator") {
+		window.open('https://itch.io/jam/game-off-2021/rate/1286854', '_blank').focus();	}
+	
+	if (redirectTarget == "bug-android") {
+		window.open('https://play.google.com/store/apps/details?id=com.a.a.s.developmentstudios.githubgameoff2021', '_blank').focus();	}
+	
+	if (redirectTarget == "bug-apple") {
+		window.open('https://apps.apple.com/us/app/buginator/id1597865535', '_blank').focus();	}	
+		
+}
+
+
 
 /***********************************************/
 /*****************  Popup Form  ****************/
